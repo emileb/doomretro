25643814171312e9591bd9f2d8ebf8ec5f97995a
Emile Belanger <emile.belanger@gmail.com>
2018-06-25 19:17:22 +0100

    Moved common interface

--- mobile/Android.mk
+++ mobile/Android.mk
@@ -12,12 +12,12 @@ LOCAL_C_INCLUDES :=     $(SDL_INCLUDE_PATHS)  \
                         $(TOP_DIR) \
                         $(TOP_DIR)/MobileTouchControls \
                         $(TOP_DIR)/Clibs_OpenTouch \
-                        $(TOP_DIR)/Doom/common_interface \
+                        $(TOP_DIR)/Clibs_OpenTouch/idtech1 \
                         $(LOCAL_PATH)/../mobile \
 
 ANDROID_FILES = \
-      ../../common_interface/android_jni.cpp \
-      ../../common_interface/touch_interface.cpp \
+      ../../../Clibs_OpenTouch/idtech1/android_jni.cpp \
+      ../../../Clibs_OpenTouch/idtech1/touch_interface.cpp \
       ../mobile/game_interface.c \
 
 FILE_LIST := $(wildcard $(LOCAL_PATH)*.c)
