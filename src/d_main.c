c9cca105e88fd5b8276004bcbe94dfb742e8f2b1
Emile Belanger <emile.belanger@gmail.com>
2026-08-31 10:12:49 +0100

    Fix missing protos

--- src/d_main.c
+++ src/d_main.c
@@ -491,6 +491,7 @@ void D_Display(void)
         // normal update
 
 #ifdef __ANDROID__ // The touch controls change the viewport, call this to fix. This function does not exist in SDL2
+        int SDL_ForceupdateViewport(SDL_Renderer * renderer);
         SDL_ForceupdateViewport(renderer);
 #endif
 
@@ -527,6 +528,7 @@ void D_Display(void)
         done = Wipe_ScreenWipe();
 
 #ifdef __ANDROID__ // The touch controls change the viewport, call this to fix. This function does not exist in SDL2
+        int SDL_ForceupdateViewport(SDL_Renderer * renderer);
         SDL_ForceupdateViewport(renderer);
 #endif
 
